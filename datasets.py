be27fcfa559955c906501634d6759bcbbd434379
Alex Andonian <alexandonian@gmail.com>
2020-01-02 21:16:25 -0500

    Fix make_hdf5 bug

--- datasets.py
+++ datasets.py
@@ -21,7 +21,7 @@ from torchvision import transforms
 from tqdm import tqdm
 
 import cfg
-from utils import MultiEpochSampler
+# from utils import MultiEpochSampler
 
 try:
     getattr(torch.hub, 'HASH_REGEX')
@@ -628,12 +628,8 @@ def _make_hdf5(dataloader, root, filename, chunk_size=500, compression=False):
                                              compression=compression)
                 print('Image chunks chosen as {}'.format(imgs_dset.chunks))
                 imgs_dset[...] = x
-                if len(y.shape) > 1:
-                    maxshape = (dataset_len,) + y.shape[1:]
-                    chunks = (chunk_size,) + y.shape[1:]
-                else:
-                    maxshape = (dataset_len,)
-                    chunks = (chunk_size,)
+                maxshape = (dataset_len,)
+                chunks = (chunk_size,)
                 labels_dset = f.create_dataset('labels', y.shape, dtype='int64',
                                                maxshape=maxshape,
                                                chunks=chunks,
